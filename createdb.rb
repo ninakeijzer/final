b279b67feb4b09f399a07b1c6a4d1471bcd5faa3
ninakeijzer <nina_keijzer@hotmail.com>
2020-03-13 16:52:30 +0000

    updates createdb, gitignore, gemfile

--- createdb.rb
+++ createdb.rb
@@ -1,10 +1,27 @@
 # Set up for the application and database. DO NOT CHANGE. #############################
 require "sequel"                                                                      #
 connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
-DB = Sequel.connect(connection_string)                                                #
+DB ||= Sequel.connect(connection_string)                                              #
 #######################################################################################
 
 # Database schema - this should reflect your domain model
+# DB.create_table! :events do
+#   primary_key :id
+#   String :title
+#   String :description, text: true
+#   String :date
+#   String :location
+# end
+# DB.create_table! :rsvps do
+#   primary_key :id
+#   foreign_key :event_id
+#   Boolean :going
+#   String :name
+#   String :email
+#   String :comments, text: true
+# end
+
+# New domain model - adds users
 DB.create_table! :events do
   primary_key :id
   String :title
@@ -15,21 +32,26 @@ end
 DB.create_table! :rsvps do
   primary_key :id
   foreign_key :event_id
+  foreign_key :user_id
   Boolean :going
+  String :comments, text: true
+end
+DB.create_table! :users do
+  primary_key :id
   String :name
   String :email
-  String :comments, text: true
+  String :password
 end
 
 # Insert initial (seed) data
 events_table = DB.from(:events)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+events_table.insert(title: "Rescue grocery perishables", 
+                    description: "Every day of the week, Valli's offers packages of fresh produce that are close to losing their freshness. Each package costs $1.49, content of the packages vary depending on what needs to go!",
+                    location: "Valli Produce, Evanston")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+events_table.insert(title: "Recycle your grocery plastic bags", 
+                    description: "Jewel Osco collects used plastic grocery bags and gives them a new meaningful life.",
+                    location: "Jewel Osco, Evanston")
+
+                    puts "success!"
